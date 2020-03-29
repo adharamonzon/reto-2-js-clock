7c479cb47ccb043cbe469021bcb9861d7fe4e48d
adharamonzon <adharamonzon@gmail.com>
2020-03-29 16:09:19 +0200

    solve painting problem, add styles to hourHand and secondHand

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
 'strict use';
 
-const hourHand = document.querySelector('.js-hour-hand');
-const minHand = document.querySelector('.js-min-hand');
 const secondHand = document.querySelector('.js-sec-hand');
+const minHand = document.querySelector('.js-min-hand');
+const hourHand = document.querySelector('.js-hour-hand');
 
 function setDate() {
   /* debugger; */
   let now = new Date();
 
   const seconds = now.getSeconds(); //nos devuelve los segundos de un minuto
-  const secondsDegrees = (seconds / 60) * 360; // cada segundo se multiplica por los 360 grados de la esfera del reloj
-  secondHand.style.transform = `rotate (${secondsDegrees})`;
+  const secondsDegrees = (seconds / 60) * 360 + 180; // cada segundo se multiplica por los 360 grados de la esfera del reloj
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
   console.log(seconds);
 
   const minutes = now.getMinutes();
-  const minutesDegrees = (minutes / 60) * 360 + 180;
-  minHand.style.transform = `rotate (${minutesDegrees})`;
+  const minutesDegrees = (minutes / 60) * 360 + (seconds / 60) * 6 + 180;
+  minHand.style.transform = `rotate(${minutesDegrees}deg)`;
   console.log(minutes);
 
   const hour = now.getHours();
-  const hoursDegrees = (hour / 12) * 360 + 180;
-  hourHand.style.transform = `rotate (${hoursDegrees})`;
+  const hoursDegrees = (hour / 12) * 360 + (seconds / 60) * 30 + 180;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
   console.log(hour);
 }
 
